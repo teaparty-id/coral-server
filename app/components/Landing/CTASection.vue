cefb5d6336fb856e807ad19b7f218241bbe577d5
Dicky Mulia Fiqri <santrialanwari@gmail.com>
2026-06-27 21:20:49 +0700

    fix: adjust padding and opacity in CTA section for improved layout

--- app/components/Landing/CTASection.vue
+++ app/components/Landing/CTASection.vue
@@ -2,10 +2,10 @@
   <section
     class="flex justify-center items-center bg-linear-65 from-primary to-pink-500 text-primary-content min-h-124"
   >
-    <div class="text-center lg:text-start lg:border lg:border-base rounded-2xl p-18">
+    <div class="text-center lg:text-start lg:border lg:border-base rounded-2xl p-4 lg:p-18">
       <h2 class="text-5xl">Siap Meninggalkan Sistem Timer Lama?</h2>
 
-      <p class="text-xl mt-6 opacity-90">Modernisasi operasional rental PlayStation Anda dengan Coraline.</p>
+      <p class="text-xl mt-6">Modernisasi operasional rental PlayStation Anda dengan Coraline.</p>
 
       <NuxtLink to="#download">
         <button class="btn btn-outline btn-lg mt-10" data-theme="coraline-dark">Mulai Sekarang</button>
